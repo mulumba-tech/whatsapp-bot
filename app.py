6f960ac121dadcb374fe9fc837b0eee416760cda
mulumba-tech <najiibmulumba82@gmail.com>
2026-03-29 21:36:15 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    
+    print("Received:", data)
+
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
